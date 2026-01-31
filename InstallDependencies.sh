6ecc07a5a538c3b48d10166394b8cab54b640e27
Erfan Bahramali <ErfanBahramali@gmail.com>
2026-02-01 00:53:58 +0330

    Add DNS-over-SOCKS proxy implementation and related files
    - Introduced a new DNS-over-SOCKS proxy application with core functionality in dns_proxy.c.
    - Added configuration file (dns_proxy.conf) for customizable settings.
    - Created systemd service file (tcpdnsproxy.service) for managing the proxy as a service.
    - Implemented a Makefile for building the proxy application.
    - Added .gitignore to exclude proxy log files.
    - Included example resolv.conf with DNS server addresses.
    - Updated socks2vpn.sh script to integrate with the new DNS proxy and manage routes.

---
+++ InstallDependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if ! command -v route &>/dev/null; then
+    echo "net-tools is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y net-tools
+    echo "net-tools installed successfully!"
+fi
+
+if ! command -v sshpass &>/dev/null; then
+    echo "sshpass is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y sshpass
+    echo "sshpass installed successfully!"
+fi
+
+if ! command -v autossh &>/dev/null; then
+    echo "autossh is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y autossh
+    echo "autossh installed successfully!"
+fi
+
+if ! command -v proxychains4 &>/dev/null; then
+    echo "proxychains4 is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y proxychains4
+    echo "proxychains4 installed successfully!"
+fi
--- socks2vpn.sh
+++ socks2vpn.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
-# https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/dns-over-https-client/
+# Add executable permission to the script:
+# chmod +x ~/Desktop/FDIRV/socks2vpn.sh
 
-if ! command -v route &> /dev/null; then
-    echo "net-tools is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y net-tools
-    echo "net-tools installed successfully!"
+# Add alias in:
+# ~/.zshrc
+# # FDIRV
+# alias vpn="sudo ~/Desktop/FDIRV/socks2vpn.sh"
+
+# Password-free and safe this file in:
+# /etc/sudoers
+# sudo visudo
+# #FDIRV
+# user ALL=(ALL) NOPASSWD: ~/Desktop/FDIRV/socks2vpn.sh
+
+# script dir for open script in other directory
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+XRAY_PATH="$SCRIPT_DIR/Xray"
+XRAY_BASE_CONFIG_PATH="$XRAY_PATH/config.base.json"
+XRAY_OUTBOUNDS_PATH="$XRAY_PATH/outbounds.json"
+XRAY_CONFIG_PATH="$XRAY_PATH/config.json"
+DOMAINS_PATH="$SCRIPT_DIR/domains.txt"
+IPS_PATH="$SCRIPT_DIR/ips.txt"
+
+jq --slurpfile outs $XRAY_OUTBOUNDS_PATH '.outbounds = $outs[0]' $XRAY_BASE_CONFIG_PATH > $XRAY_CONFIG_PATH
+
+PROXY_ADDRESSES=$(jq '.[0].settings.[].[0].address' $XRAY_OUTBOUNDS_PATH | cut -d "\"" -f 2)
+# PROXY_ADDRESSES=''
+
+if [[ $PROXY_ADDRESSES != null && ! $PROXY_ADDRESSES =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+
+    # is not ip
+    # is domain
+    PROXY_ADDRESSES=$(dig +short $PROXY_ADDRESSES | xargs)
 fi
 
-if ! command -v sshpass &> /dev/null; then
-    echo "sshpass is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y sshpass
-    echo "sshpass installed successfully!"
-fi
+# Unique domains
+DOMAINS=$(grep -v '^#' "$DOMAINS_PATH" | grep -v '^$' | sort -u)
 
-if ! command -v autossh &> /dev/null; then
-    echo "autossh is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y autossh
-    echo "autossh installed successfully!"
-fi
+# Resolve domains in parallel
+DOMAINS_IPS=$(echo "$DOMAINS" | xargs -n1 -P25 dig +short +time=6 +tries=1 | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$')
 
-if ! command -v proxychains &> /dev/null; then
-    echo "proxychains is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y proxychains
-    echo "proxychains installed successfully!"
-fi
+# Unique IPs
+IPS=$(grep -v '^#' "$IPS_PATH" | grep -v '^$' | sort -u)
 
-# For add exception:
-# sudo ip route add 172.217.18.14 via 192.168.0.254 dev enp5s0
+# Add IPs to PROXY_ADDRESSES
+PROXY_ADDRESSES="$PROXY_ADDRESSES $DOMAINS_IPS $IPS"
 
-# set proxy in:
-# /etc/proxychains.conf (127.0.0.1:2080)
-IP=$(nslookup example.com | grep 'Address: ' | awk '{print $2}')
-#IP=$(proxychains curl 'https://api.ipify.org' | sed -n '2,$p')
-PROXY_ADDRESS=$IP
-#PROXY_ADDRESS=''
-
-echo "Ip: $IP"
-
-DNS_PRIMARY="1.1.1.1"
-DNS_SECONDARY="1.0.0.1"
-DNS_PRIMARY_V6="2606:4700:4700::1111"
-DNS_SECONDARY_V6="2606:4700:4700::1001"
+# Unique all addresses
+PROXY_ADDRESSES=$(echo "$PROXY_ADDRESSES" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
 # SSH
 SERVER_ADDRESS=""
@@ -53,98 +57,220 @@ SSH_PORT=
 SSH_USERNAME=""
 SSH_PASSWORD=""
 UDPGW_PORT=
+SOCKS_ADDRESS="127.0.0.1:1090"
 
+DNS_PRIMARY="1.1.1.1"
+DNS_SECONDARY="1.0.0.1"
+DNS_PRIMARY_V6="2606:4700:4700::1111"
+DNS_SECONDARY_V6="2606:4700:4700::1001"
+
+# Network
 GATEWAY=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
 INTERFACE=$(ip addr show | awk '/inet.*brd/{print $NF; exit}')
+
+# Tun
 TUN_NAME="tun0"
 TUN_ADDRESS="10.10.10.10"
-SOCKS_ADDRESS="0.0.0.0:1090"
 
-# RESOLV_CONF_PATH="/etc/resolv.conf"
-
-# if ! grep -q "nameserver 127.0.0.1" $RESOLV_CONF_PATH; then
-#     sed -i "s/nameserver/# nameserver/g" $RESOLV_CONF_PATH
-#     sed -i "s/options/# options/g" $RESOLV_CONF_PATH
-#     sed -i "s/search/# search/g" $RESOLV_CONF_PATH
-#     # echo -e "\nnameserver 127.0.0.1" >> $RESOLV_CONF_PATH
-#     echo -e "\nnameserver 1.1.1.1" >> $RESOLV_CONF_PATH
-# fi
-
-proxychains sshpass -p "$SSH_PASSWORD" ssh $SSH_USERNAME@$SERVER_ADDRESS -p $SSH_PORT -ND $SOCKS_ADDRESS & SSH_PID=$!
-
-sleep 2
-
-PROXY_ROUTE="$PROXY_ADDRESS via $GATEWAY"
-SERVER_ROUTE="$SERVER_ADDRESS via $GATEWAY"
+# Route
 TUN_ROUTE="default via $TUN_ADDRESS dev $TUN_NAME metric 1"
 
-setupTunDevice() {
+# ProxyChains proxy
+PROXYCHAINS_PROXY_HOST="127.0.0.1"
+PROXYCHAINS_PROXY_PORT="10808"
+
+# Create temporary proxychains config
+PROXYCHAINS_CONFIG="$SCRIPT_DIR/proxychains.conf"
+
+cat > "$PROXYCHAINS_CONF" << EOF
+strict_chain
+proxy_dns
+remote_dns_subnet 224
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+
+[ProxyList]
+socks5 $PROXYCHAINS_PROXY_HOST $PROXYCHAINS_PROXY_PORT
+EOF
+
+executeXray() {
+
+    $XRAY_PATH/xray run -c $XRAY_CONFIG_PATH >/dev/null &
+    XRAY_PID=$!
+
+    echo "Xray Executed ..."
+
+    sleep 2
+}
+
+executeSSH() {
+
+    {
+        proxychains -f "$PROXYCHAINS_CONF" sshpass -p "$SSH_PASSWORD" ssh $SSH_USERNAME@$SERVER_ADDRESS -p $SSH_PORT -ND $SOCKS_ADDRESS
+    } &>/dev/null &
+    SSH_PID=$!
+
+    echo "SSH Executed ..."
+
+    sleep 2
+
+}
+
+executeDNS() {
+
+    # resolvectl domain $TUN_NAME "~."
+    # resolvectl default-route $TUN_NAME true
+    # resolvectl dns $TUN_NAME $DNS_PRIMARY $DNS_SECONDARY $DNS_PRIMARY_V6 $DNS_SECONDARY_V6
+    # resolvectl default-route $INTERFACE false
+
+    CURRENT_DNS=$(cat /etc/resolv.conf)
+
+    truncate --size 0 $SCRIPT_DIR/dns-tcp-socks-proxy/proxy.log
+
+    $SCRIPT_DIR/dns-tcp-socks-proxy/dns_proxy $SCRIPT_DIR/dns-tcp-socks-proxy/dns_proxy.conf &>/dev/null &
+
+    echo "DNS Executed ..."
+
+}
+
+stopSSH() {
+
+    if ps -p $SSH_PID >/dev/null; then
+
+        kill -9 $SSH_PID
+
+    fi
+
+    echo "SSH Killed."
+
+}
+
+stopXray() {
+
+    if ps -p $XRAY_PID >/dev/null; then
+
+        kill -9 $XRAY_PID
+
+    fi
+
+    echo "Xray Killed."
+}
+
+stopDNS() {
+
+    # resolvectl default-route $INTERFACE true
+
+    DNS_PID=$(pgrep -x dns_proxy | xargs)
+
+    kill -9 $DNS_PID
+
+    echo "DNS Killed."
+
+    echo "$CURRENT_DNS" >/etc/resolv.conf
+
+    echo "DNS Restored."
+
+}
+
+addTuntap() {
+
     TUN_DEVICE=$(ip address | grep "$TUN_NAME")
-    if [ -z "$TUN_DEVICE" ]
-    then
+
+    if [ -z "$TUN_DEVICE" ]; then
+
         ip tuntap add mode tun dev "$TUN_NAME"
+
         ip address add "$TUN_ADDRESS/24" dev "$TUN_NAME"
+
         ip link set dev "$TUN_NAME" up
+
+        echo "Tuntap Added."
+
     fi
 }
 
-addRoute() {
-    FIND=$(ip route | grep "$1")
-    if [ -z "$FIND" ]
-    then
-        ip route add $1
-    fi
-}
+deleteTuntap() {
 
-delRoute() {
-    FIND=$(ip route | grep "$1")
-    if [ -n "$FIND" ]
-    then
-        ip route del $1
+    TUN_DEVICE=$(ip address | grep "$TUN_NAME")
+
+    if [ "$TUN_DEVICE" ]; then
+
+        ip tuntap del mode tun dev $TUN_NAME
+
+        echo "Tuntap Deleted."
+
     fi
 }
 
 start() {
-    setupTunDevice
-    addRoute "$PROXY_ROUTE"
-    addRoute "$SERVER_ROUTE"
-    addRoute "$TUN_ROUTE"
 
-    # echo "Handle DNS"
-    resolvectl domain $TUN_NAME "~."
-    resolvectl default-route $TUN_NAME true
-    resolvectl dns $TUN_NAME $DNS_PRIMARY $DNS_SECONDARY $DNS_PRIMARY_V6 $DNS_SECONDARY_V6
-    resolvectl default-route $INTERFACE false
+    echo "Starting ..."
 
-    # tun2socks -device "tun://$TUN_NAME" -proxy "socks5://$SOCKS_ADDRESS" -interface "$INTERFACE" -mtu 1500 -tcp-sndbuf 2048k -tcp-rcvbuf 2048k -tcp-auto-tuning
-    # ./badvpn-tun2socks --tundev $TUN_NAME --netif-ipaddr $TUN_ADDRESS --netif-netmask 255.255.255.0 --socks-server-addr $SOCKS_ADDRESS
-    ./badvpn-tun2socks --tundev $TUN_NAME --netif-ipaddr $TUN_ADDRESS --netif-netmask 255.255.255.0 --socks-server-addr $SOCKS_ADDRESS --udpgw-remote-server-addr 127.0.0.1:$UDPGW_PORT
+    executeXray
+
+    executeSSH
+
+    executeDNS
+
+    addTuntap
+
+    for PROXY_ADDRESS in $PROXY_ADDRESSES; do
+
+        ip route add $PROXY_ADDRESS via $GATEWAY
+
+    done
+
+    echo "PROXY_ROUTE Added."
+
+    ip route add $TUN_ROUTE
+
+    echo "TUN_ROUTE Added."
+
+    # $SCRIPT_DIR/tun2socks -device "tun://$TUN_NAME" -proxy "socks5://$SOCKS_ADDRESS" -interface "$INTERFACE" -mtu 1500 -tcp-sndbuf 2048k -tcp-rcvbuf 2048k -tcp-auto-tuning
+
+    $SCRIPT_DIR/badvpn-tun2socks --tundev $TUN_NAME --netif-ipaddr $TUN_ADDRESS --netif-netmask 255.255.255.0 --socks-server-addr $SOCKS_ADDRESS --udpgw-remote-server-addr 127.0.0.1:$UDPGW_PORT
+
+    echo "Success."
 }
 
 stop() {
-    kill -9 "$SSH_PID"
-    delRoute "$PROXY_ROUTE"
-    delRoute "$TUN_ROUTE"
-    delRoute "$SERVER_ROUTE"
 
-    echo "Restore DNS"
-    resolvectl default-route $INTERFACE true
+    echo "Stoping ..."
 
-    # # sed -i "s/nameserver 127.0.0.1//g" $RESOLV_CONF_PATH
-    # sed -i "s/nameserver 1.1.1.1//g" $RESOLV_CONF_PATH
-    # sed -i '/^$/d' $RESOLV_CONF_PATH
-    # sed -i "s/# nameserver/\nnameserver/g" $RESOLV_CONF_PATH
-    # sed -i "s/# options/options/g" $RESOLV_CONF_PATH
-    # sed -i "s/# search/search/g" $RESOLV_CONF_PATH
+    ip route del $TUN_ROUTE
+
+    echo "TUN_ROUTE Deleted."
+
+    for PROXY_ADDRESS in $PROXY_ADDRESSES; do
+
+        ip route del $PROXY_ADDRESS via $GATEWAY
+
+    done
+
+    echo "PROXY_ROUTE Deleted."
+
+    deleteTuntap
+
+    stopDNS
+
+    stopSSH
+
+    stopXray
+
+    # Clean up temporary proxychains config
+    rm -f "$PROXYCHAINS_CONFIG"
+
+    echo "Success."
+
 }
 
-if [ "$EUID" -ne 0 ]
-then
+if [ "$EUID" -ne 0 ]; then
+
     echo "Please run as root"
+
     exit
 fi
 
 trap stop EXIT
+
 start
-
-
